f487941af0f00d1ace5ccf2418de43db55d19a7c
yangzj <gogoyangzaiju@126.com>
2017-02-20 15:15:05 +0800

    printf

---
+++ 20170220/printf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+printf "%-5s %-10s %-4s\n" No Name Score
+printf "%-5s %-10s %-4.2f\n" 1 xiaoming 89.3254
+printf "%-5s %-10s %-4.2f\n" 2 xiaomei 59.6546
+printf "%-5s %-10s %-4.2f\n" 3 xiaomjun 69.1114
+printf "%-5s %-10s %-4.2f\n" 4 xiaomhua 79.9954
+
+# ohter:
+# chmod 775 ./xxx
+# chmod a+x ./xxx
+# chmod u+rwx ./xxx
+# chmod ug+x ./xxx
+# chmod o-x ./xxx
